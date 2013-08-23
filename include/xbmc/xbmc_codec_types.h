8cca4092c4c8934db859d39f23871460c1a104f4
Anton Fedchin <afedchin@ruswizards.com>
2013-08-23 15:41:12 +0400

    sync pvr api.

--- include/xbmc/xbmc_codec_types.h
+++ include/xbmc/xbmc_codec_types.h
@@ -1,4 +1,6 @@
-#pragma once
+#ifndef __XBMC_CODEC_TYPES_H__
+#define __XBMC_CODEC_TYPES_H__
+
 /*
  *      Copyright (C) 2005-2013 Team XBMC
  *      http://xbmc.org
@@ -19,27 +21,34 @@
  *
  */
 
-#ifndef __XBMC_SCR_H__
-#define __XBMC_SCR_H__
+#ifdef __cplusplus
+extern "C" {
+#endif
 
-#include "xbmc_addon_dll.h"
-#include "xbmc_scr_types.h"
+typedef unsigned int xbmc_codec_id_t;
 
-extern "C"
+typedef enum
 {
+    XBMC_CODEC_TYPE_UNKNOWN = -1,
+    XBMC_CODEC_TYPE_VIDEO,
+    XBMC_CODEC_TYPE_AUDIO,
+    XBMC_CODEC_TYPE_DATA,
+    XBMC_CODEC_TYPE_SUBTITLE,
+    XBMC_CODEC_TYPE_NB
+} xbmc_codec_type_t;
 
-  // Functions that your visualisation must implement
-  void Start();
-  void Render();
-  void GetInfo(SCR_INFO* pInfo);
+typedef struct
+{
+  xbmc_codec_type_t codec_type;
+  xbmc_codec_id_t   codec_id;
+} xbmc_codec_t;
 
-  // function to export the above structure to XBMC
-  void __declspec(dllexport) get_addon(struct ScreenSaver* pScr)
-  {
-    pScr->Start = Start;
-    pScr->Render = Render;
-    pScr->GetInfo = GetInfo;
-  };
+#define XBMC_INVALID_CODEC_ID   0
+#define XBMC_INVALID_CODEC      { XBMC_CODEC_TYPE_UNKNOWN, XBMC_INVALID_CODEC_ID }
+
+#ifdef __cplusplus
 };
+#endif
 
 #endif
+
